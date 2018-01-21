fdfa679f885ae6a274504308da7d76130ce9cd12
Mischa Spiegelmock <revmischa@cpan.org>
2018-01-21 07:06:06 -0800

    oauth

--- index.py
+++ index.py
@@ -2,7 +2,24 @@ import json
 import datetime
 import requests
 import random
+import boto3
 from urllib.parse import parse_qs
+from pprint import pprint
+
+# AWS SSM encrypted parameter store
+ssm = boto3.client('ssm', region_name='ap-southeast-1')
+def get_ssm_param(param_name: str) -> str:
+    """Get an encrypted AWS Systems Manger secret."""
+    response = ssm.get_parameters(
+        Names=[param_name],
+        WithDecryption=True,
+    )
+    if not response['Parameters'] or not response['Parameters'][0] or not response['Parameters'][0]['Value']:
+        raise Exception(f"Configuration error: missing AWS SSM parameter: {param_name}")
+    return response['Parameters'][0]['Value']
+
+SLACK_OAUTH_CLIENT_ID = get_ssm_param('ud_slack_oauth_client_id')
+SLACK_OAUTH_CLIENT_SECRET = get_ssm_param('ud_slack_oauth_client_secret')
 
 
 def respond(err=None, res=None):
@@ -58,9 +75,33 @@ def slack_slash(event):
         ]
     })
 
-def oauth(event):
+def oauth_begin(event):
     return {
-        'statusCode': 301,
+        'statusCode': 302,
+        'body': "Redirecting...",
+        'headers': {
+            'Location': 'https://slack.com/oauth/authorize?client_id=2716024371.296705110721&scope=commands',
+        },
+    }
+
+
+def oauth(event):
+    args = event['queryStringParameters']
+    code = args['code']
+    print(f"code; {code}")
+    # get auth token
+    res = requests.get('https://slack.com/api/oauth.access', params={
+        'code': code,
+        'client_id': SLACK_OAUTH_CLIENT_ID,
+        'client_secret': SLACK_OAUTH_CLIENT_SECRET,
+        'redirect_uri': 'https://singapi.mischa.lol/udv1/oauth',
+    }).json()
+
+    print(res)
+    return "Success!"
+
+    return {
+        'statusCode': 302,
         'body': "Installed!",
         'headers': {
             'Location': 'https://github.com/revmischa/slack-urbandictionary#usage',
@@ -70,6 +111,8 @@ def oauth(event):
 def handler(event, context):
     if event['path'] == '/oauth':
         return oauth(event)
+    elif event['path'] == '/install':
+        return oauth_begin(event)
     elif event['path'] == '/':
         return slack_slash(event)
     else:
